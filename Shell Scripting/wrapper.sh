79c45193ede397d3197892b6e3151d24587e5ce6
amlandas123 <56760388+amlandas123@users.noreply.github.com>
2024-01-30 11:57:12 +0530

    From Amlan

--- Shell Scripting/wrapper.sh
+++ Shell Scripting/wrapper.sh
@@ -1,5 +1,16 @@
 
 #!/bin/bash
-echo -e "\e[32m Enter your name:  \e[0m"
+echo -e "\e[33m Enter your name: \e[0m"
 read name
+echo -e "\e[33m Enter your group name: \e[0m"
+read gname
+
+if [ $?=0 ]; then
+    useradd -G $gname $name
+    echo -e "\e[36m User $name has been added"
+else
+    echo " Enter username and group name
+fi
+
+
 
