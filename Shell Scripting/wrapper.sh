d5668d3f1991e9b3329ee86c1f44a5cd13d7090d
amlandas123 <56760388+amlandas123@users.noreply.github.com>
2024-01-30 13:09:36 +0530

    From Amlan

--- Shell Scripting/wrapper.sh
+++ Shell Scripting/wrapper.sh
@@ -5,12 +5,12 @@ echo -e "\e[33m Your entered name is : $1 \e[0m"
 echo -e "\e[33m Your entered  group name: $2 \e[0m"
 read gname
 
-if [ -z $1 ] || [ -z $2 ]; then
-    echo -e "\e[36m Enter correct name"
-else
+if [ -n "$1" ] && [ -n "$2" ]; then
     groupadd $gname
     useradd -G $gname $name
     echo -e "\e[36m User $name has been added"
+else
+    echo -e "\e[32m You have not entered data in correct format \e[0m"
 fi
 
 
